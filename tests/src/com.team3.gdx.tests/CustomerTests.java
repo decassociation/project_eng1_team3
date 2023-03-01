e9789ceaf3cdab354d12858dd7f5e41de1e62d58
OwenLister-GitHub <owenl9.ol@gmail.com>
2023-03-01 11:28:11 +0000

    Adding 4 versioned files. Implemented some more tests for the customer spawning feature.

--- tests/src/com.team3.gdx.tests/CustomerTests.java
+++ tests/src/com.team3.gdx.tests/CustomerTests.java
@@ -20,11 +20,41 @@ public class CustomerTests {
 
         TiledMap map1 = new TmxMapLoader().load("map/art_map/customertest.tmx");
         CustomerController cc = new CustomerController(map1);
-        assertNull("There should not be any customers spawned/existing but at least one exists", cc.customers[0]);
+        // Test when no customers exist:
+        assertTrue("There should not be any customers spawned/existing but at least one exists", cc.customers[0]==null
+                && cc.customers[1]==null && cc.customers[2]==null && cc.customers[3]==null && cc.customers[4]==null);
+
+        // Test when 1 customer exists
         cc.spawnCustomer();
         assertTrue("A customer should have been spawned so X.customers[0] should be of type Customer" +
-                "but is not", cc.customers[0] instanceof Customer); // This is highlighted as redundant but ISN'T!
+                "but is not", cc.customers[0] instanceof Customer ); // This is highlighted as redundant but ISN'T!
 
+        assertTrue("A single customer should have been spawned but either a customer hasn't been spawned" +
+                "or multiple customers have been spawned", cc.customers[0] instanceof Customer
+                && cc.customers[1]==null && cc.customers[2]==null && cc.customers[3]==null && cc.customers[4]==null);
+                // This is highlighted as redundant but ISN'T!
+
+        // Test when 2 customers exists
+        cc.spawnCustomer();
+        assertTrue("A customer should have been spawned so X.customers[1] should be of type Customer" +
+                "but is not", cc.customers[1] instanceof Customer );
+
+        // Test when 3 customers exists
+        cc.spawnCustomer();
+        assertTrue("A customer should have been spawned so X.customers[2] should be of type Customer" +
+                "but is not", cc.customers[2] instanceof Customer );
+
+        // Test when 4 customers exists
+        cc.spawnCustomer();
+        assertTrue("A customer should have been spawned so X.customers[3] should be of type Customer" +
+                "but is not", cc.customers[3] instanceof Customer );
+
+        // Test when 5 customers exists
+        cc.spawnCustomer();
+        assertTrue("A customer should have been spawned so X.customers[4] should be of type Customer" +
+                "but is not", cc.customers[4] instanceof Customer );
     }
+
+
 
 }
