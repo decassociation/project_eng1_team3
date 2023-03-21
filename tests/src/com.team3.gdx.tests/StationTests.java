d600bc874d4a7c5c251c96dca92edc4e5d410cdc
OwenLister-GitHub <owenl9.ol@gmail.com>
2023-03-21 14:50:19 +0000

    Added test for customer serving at a serving station in testCreateServingStation()

--- tests/src/com.team3.gdx.tests/StationTests.java
+++ tests/src/com.team3.gdx.tests/StationTests.java
@@ -1,10 +1,14 @@
 package com.team3.gdx.tests;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
 import com.team3gdx.game.entity.Customer;
 import com.team3gdx.game.entity.CustomerController;
+import com.team3gdx.game.food.Ingredients;
+import com.team3gdx.game.food.Menu;
+import com.team3gdx.game.food.Recipe;
 import com.team3gdx.game.screen.GameScreen;
 import com.team3gdx.game.station.ServingStation;
 import com.team3gdx.game.station.Station;
@@ -28,6 +32,11 @@ public class StationTests {
                 StationManager.stations.get(pos) == station);
     }
 
+
+    /**
+     * This tests the creation of a serving station *AND* the serving of a customer because it's the same block of code
+     * to test both; a few extra lines are needed to test the serving functionality.
+     */
     @Test
     public void testCreateServingStation(){
         //------------------------------------------ ISOLATED CREATION -------------------------------------------------
@@ -42,12 +51,23 @@ public class StationTests {
         TiledMap testMap = new TmxMapLoader().load("map/art_map/customertest.tmx");
         GameScreen.cc = new CustomerController(testMap, "easy");
         GameScreen.cc.customers[0] = new Customer(19, 20, 20, 0);
+        GameScreen.currentWaitingCustomer = GameScreen.cc.customers[0];
         GameScreen.cc.customers[0].locked = true;
+
         GameScreen.control = new Control();
         Vector2 testPosB = new Vector2(20,20);
         StationManager testSM = new StationManager();
         testSM.checkInteractedTile("Service", testPosB);
         assertTrue("Serving station should have been instantiated at (20, 20)",
                 testSM.stations.get(testPosB) instanceof ServingStation);
+
+        //---------------------------------------- TEST CUSTOMER SERVING -----------------------------------------------
+        // To test customer serving, the code from the above section is built upon so no duplicated code
+        GameScreen.cc.customers[0].order = "Burger";
+        testSM.stations.get(testPosB).slots.push(Menu.RECIPES.get(GameScreen.cc.customers[0].order));
+        // (Above line) pushes same order as the customer used to test this, to the serving stations stack
+        ((ServingStation) testSM.stations.get(testPosB)).serveCustomer();
+        assertNull("There should be no existing customers but there is one at GameScreen.cc.customers[0]",
+                GameScreen.cc.customers[0]);
     }
 }
