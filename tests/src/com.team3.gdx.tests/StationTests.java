e34775b1b03267b8c23540dd870192d050cda59f
OwenLister-GitHub <owenl9.ol@gmail.com>
2023-03-21 12:36:33 +0000

    Finished implementation of testCreateServingStation()

--- tests/src/com.team3.gdx.tests/StationTests.java
+++ tests/src/com.team3.gdx.tests/StationTests.java
@@ -1,9 +1,15 @@
 package com.team3.gdx.tests;
 
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
+import com.team3gdx.game.entity.Customer;
+import com.team3gdx.game.entity.CustomerController;
+import com.team3gdx.game.screen.GameScreen;
 import com.team3gdx.game.station.ServingStation;
 import com.team3gdx.game.station.Station;
 import com.team3gdx.game.station.StationManager;
+import com.team3gdx.game.util.Control;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -25,12 +31,23 @@ public class StationTests {
     @Test
     public void testCreateServingStation(){
         //------------------------------------------ ISOLATED CREATION -------------------------------------------------
-        Vector2 testPos = new Vector2(12,13);
-        ServingStation testSS = new ServingStation(testPos);
-        StationManager.stations.put(testPos, testSS);
+        Vector2 testPosA = new Vector2(12,13);
+        ServingStation testSS = new ServingStation(testPosA);
+        StationManager.stations.put(testPosA, testSS);
         assertEquals("Serving station should have been instantiated at (12, 13)",
-                StationManager.stations.get(testPos), testSS);
+                StationManager.stations.get(testPosA), testSS);
 
         //----------------------------- CREATION THROUGH StationManager's checkInteractedTile --------------------------
+
+        TiledMap testMap = new TmxMapLoader().load("map/art_map/customertest.tmx");
+        GameScreen.cc = new CustomerController(testMap, "easy");
+        GameScreen.cc.customers[0] = new Customer(19, 20, 20, 0);
+        GameScreen.cc.customers[0].locked = true;
+        GameScreen.control = new Control();
+        Vector2 testPosB = new Vector2(20,20);
+        StationManager testSM = new StationManager();
+        testSM.checkInteractedTile("Service", testPosB);
+        assertTrue("Serving station should have been instantiated at (20, 20)",
+                testSM.stations.get(testPosB) instanceof ServingStation);
     }
 }
